3f4d34930962c895c207b42e402377ec940062dd
DantesBelmont <odd822@live.com>
2023-05-12 22:18:19 -0500

    HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 3, 5, 7 };
+
+            Console.WriteLine(myArray[2]);
+        }
+    }
+}
